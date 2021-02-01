da1117681b482ec63d99209bc788f5dc46fadce4
ssi91 <sergeev.s.i1991@gmail.com>
2021-02-02 04:59:10 +0700

    rename the package to avoid circular imports

---
+++ gkm.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from git_client.gkm import GitCLIClient
+
+
+def _parse_argv():
+    if sys.argv[1] == '-i':
+        return sys.argv[2], sys.argv[3:]
+    return None, sys.argv[1:]
+
+
+def _print_help():
+    pass
+
+
+try:
+    ssh_key_path, git_argv = _parse_argv()
+except IndexError:
+    _print_help()
+    exit(2)
+
+ssh_key_path = ssh_key_path or os.environ.get('GIT_SSH_KEY')
+
+git = GitCLIClient(_ssh_key_path=ssh_key_path)
+git.command(*git_argv)

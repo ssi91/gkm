c12e798aba7980ca33024492f4f934e64c8c2530
ssi91 <sergeev.s.i1991@gmail.com>
2021-03-10 16:36:26 +0700

    add some tests

--- gkm.py
+++ gkm.py
@@ -15,13 +15,14 @@ def _print_help():
     pass
 
 
-try:
-    ssh_key_path, git_argv = _parse_argv()
-except IndexError:
-    _print_help()
-    exit(2)
+if __name__ == '__main__':
+    try:
+        ssh_key_path, git_argv = _parse_argv()
+    except IndexError:
+        _print_help()
+        exit(2)
 
-ssh_key_path = ssh_key_path or os.environ.get('GIT_SSH_KEY')
+    ssh_key_path = ssh_key_path or os.environ.get('GIT_SSH_KEY')
 
-git = GitCLIClient(_ssh_key_path=ssh_key_path)
-git.command(*git_argv)
+    git = GitCLIClient(_ssh_key_path=ssh_key_path)
+    git.command(*git_argv)
--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,22 @@
 import os
+import sys
 
 from git_cli.gkm import GitCLIClient
+from gkm import _parse_argv
 
 
 def test_context_manager():
     with GitCLIClient(None, '/') as git_client:
         assert '/' == os.getcwd()
+
+
+def test__parse_argv():
+    input0 = "command -i /path/to/key some git command"
+    input1 = "command some git command"
+    argv0 = input0.split(' ')
+    argv1 = input1.split(' ')
+
+    sys.argv = argv0
+    assert ('/path/to/key', ['some', 'git', 'command']) == _parse_argv()
+    sys.argv = argv1
+    assert (None, ['some', 'git', 'command']) == _parse_argv()
